b001506cd9e3d7cbcdf17a5a2b26b379ce1bd261
Kristi <kwerry4@gmail.com>
2023-10-10 11:07:37 -0700

    more testing

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -16,7 +16,7 @@ export default function Login(props: LoginProps): JSX.Element {
       localStorage.setItem("papyrusai_access_token", location.hash.split("&")[1].split("=")[1]);
       setTimeout(() => {
         navigator("/");
-      }, 300);
+      }, 1000);
     } 
     // eslint-disable-next-line
   }, [location.hash]);

f54bd9dad71ce693fd66ff1bc33be798405968fb
Kristi <kwerry4@gmail.com>
2023-10-12 18:10:05 -0700

    another login thing

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -15,10 +15,11 @@ export default function Login(props: LoginProps): JSX.Element {
     //Currently, this page just saves the token and then navigates to the home page
     if(location.hash) {
       localStorage.setItem("papyrusai_access_token", location.hash.split("&")[1].split("=")[1]);
-    } else {
-      console.log("here TODO");
-      window.location.replace(process.env.REACT_APP_LOGIN_URL ? process.env.REACT_APP_LOGIN_URL : "");
-    }
+    } 
+    // else {
+    //   console.log("here TODO");
+    //   window.location.replace(process.env.REACT_APP_LOGIN_URL ? process.env.REACT_APP_LOGIN_URL : "");
+    // }
     navigator("/");
     // eslint-disable-next-line
   }, [location.hash]);

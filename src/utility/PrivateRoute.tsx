829744c05e359264ea9daa590b97c04bd8ecdc3d
Kristi <kwerry4@gmail.com>
2023-10-24 12:07:24 -0700

    update main menu with account/logout, styling updates, zindex, report doubles fixes

--- src/utility/PrivateRoute.tsx
+++ src/utility/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import Navigation from "../features/navigation/Navigation";
 import { UserType } from "./types/UserTypes";
@@ -14,24 +14,11 @@ interface props {
 
 
 export function PrivateRoute({ user }: props): JSX.Element {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  //create a use effect to get updated window size when user resizes window
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth)
-    }
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, []);
-
 
   return localStorage.getItem("papyrusai_access_token") && user ? (
     <>
       <Navigation />
-      <div style={windowWidth >= 1024 ?
-        { marginLeft: "17rem", width: "-webkit-fill-available", marginTop: "3rem" } :
-        { marginLeft: 0, marginTop: "3rem" }
-      }>
+      <div className="privateRoute">
         <Outlet />
       </div>
     </>

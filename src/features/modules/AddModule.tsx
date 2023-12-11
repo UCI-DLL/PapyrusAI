f127b810d167b2fee5dfc64cdf724d2979977b1a
Kristi <kwerry4@gmail.com>
2023-12-11 15:31:07 -0800

    optional documents

--- src/features/modules/AddModule.tsx
+++ src/features/modules/AddModule.tsx
@@ -90,7 +90,8 @@ export default function AddModule(): JSX.Element {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [newDoc, setNewDoc] = useState<DocumentType>({
     usageText: "Please enter your ",
-    documentType: ""
+    documentType: "",
+    optional: false,
   });
   const [promptList, setPromptList] = useState<Array<PromptType>>([]);
   const [showFullPrompts, setShowFullPrompts] = useState<boolean>(false);
@@ -424,6 +425,20 @@ The **Module Prompts** drop down shows you the various prompts, or instructions 
               }}
             />
             &nbsp;&nbsp;&nbsp;
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+              <Checkbox
+                onClick={() => {
+                  setNewDoc((prev) => ({ ...prev, optional: !newDoc.optional }));
+                }}
+                checked={newDoc.optional}
+                isDisabled={isLoading}
+              >
+                <span>
+                  Optional
+                </span>
+              </Checkbox>
+            </div>
+            &nbsp;&nbsp;&nbsp;
             <Button
               sx={{ padding: "0rem 2rem" }}
               variant="contained"
@@ -432,7 +447,7 @@ The **Module Prompts** drop down shows you the various prompts, or instructions 
                   ...prev,
                   documents: [...session.documents, newDoc]
                 }))
-                setNewDoc({ documentType: "", usageText: "Please enter your " });
+                setNewDoc({ documentType: "", usageText: "Please enter your ", optional: false });
               }}
             >
               Add
@@ -446,9 +461,10 @@ The **Module Prompts** drop down shows you the various prompts, or instructions 
           {session.documents.map((document, index) => {
             return (
               <div className="modules__documentslist" key={index}>
-                <div>
-                  <div>{document.documentType}</div>
-                  <div>{document.usageText}</div>
+                <div style={{ paddingTop: "0.3rem" }}>
+                  <div>Document Type: {document.documentType}</div>
+                  <div>Usage Text: {document.usageText}</div>
+                  <div>{document.optional ? "Optional" : "Required"}</div>
                 </div>
                 <Tooltip title="Remove">
                   <IconButton

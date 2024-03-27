fa2a7dfa0cdf49f9e775fbacfe86495da670220b
Kristi <kwerry4@gmail.com>
2024-03-26 18:38:00 -0700

    testing login token

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -12,6 +12,7 @@ export default function Login(props: LoginProps): JSX.Element {
   let navigator = useNavigate();
 
   useEffect(() => {
+    console.log("here", location.hash)
     //Currently, this page just saves the token and then navigates to the home page
     if(location.hash) {
       localStorage.setItem("papyrusai_access_token", location.hash.split("&")[1].split("=")[1]);

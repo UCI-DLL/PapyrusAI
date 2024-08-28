ef38d1256c752519b6ae3b76837a30eb95fafc38
Kristi <kwerry4@gmail.com>
2024-08-27 19:01:21 -0700

    remove console.logs

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -14,7 +14,6 @@ export default function Login(props: LoginProps): JSX.Element {
   useEffect(() => {
     //Currently, this page just saves the token and then navigates to the home page
     if (location.hash) {
-      console.log(location.hash)
       if (location.hash.split("#")[1].split("=")[0] === "error_description") {
         setTimeout(() => {
           navigator('/login-error', { state: { message: location.hash.split("#")[1].split("=")[1].split("&")[0].replaceAll("+", " ") } });
@@ -27,7 +26,6 @@ export default function Login(props: LoginProps): JSX.Element {
       }
     }
     else if (!localStorage.getItem("papyrusai_access_token")) {
-      console.log("here")
       window.location.replace(process.env.REACT_APP_LOGIN_URL ? process.env.REACT_APP_LOGIN_URL : "");
     }
     else {

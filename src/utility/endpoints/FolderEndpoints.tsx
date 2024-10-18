b637804d38860d944d013648b836bd303515cbf4
Kristi <kwerry4@gmail.com>
2024-10-18 10:17:02 -0700

    update endpoints

--- src/utility/endpoints/FolderEndpoints.tsx
+++ src/utility/endpoints/FolderEndpoints.tsx
@@ -131,14 +131,6 @@ export function getSignedS3BucketUploadUserFolder(folderId: string, fileId: stri
   return `ragfile/upload/${folderId}/${fileId}?organization=${process.env.REACT_APP_ORGANIZATION}`;
 }
 
-export function getSignedS3BucketUpdateUploadOrgFolder(folderId: string, fileId: string) {
-  return `ragfile/upload/org/${process.env.REACT_APP_ORGANIZATION}/${folderId}/${fileId}?organization=${process.env.REACT_APP_ORGANIZATION}`;
-}
-
-export function getSignedS3BucketUpdateUploadUserFolder(folderId: string, fileId: string) {
-  return `ragfile/upload/${folderId}/${fileId}?organization=${process.env.REACT_APP_ORGANIZATION}`;
-}
-
 export function getSignedS3BucketDownloadFile(fileId: string) {
   return `ragfile/download/${fileId}?organization=${process.env.REACT_APP_ORGANIZATION}`;
 }

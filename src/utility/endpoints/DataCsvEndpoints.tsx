337e88b6682e1f921bbbe0e98a425f63215be360
Gerardo Lopez Jr. <gjlopez2@uci.edu>
2025-04-20 20:01:16 -0700

    Paginate through data endpoint
    Instead of a filler output for CSV mode, it now paginates through the data endpoint until complete. Ready for testing.

--- src/utility/endpoints/DataCsvEndpoints.tsx
+++ src/utility/endpoints/DataCsvEndpoints.tsx
@@ -1,8 +1,8 @@
 
 
-export function getAllData2(courseIds: string[], lastKeyId?: string, lastModuleId?: string): string {
+export function getAllData2(courseIds: string[], lastKeyId?: string|undefined, lastModuleId?: string|undefined): string {
   let url = `data?organization=${process.env.REACT_APP_ORGANIZATION}`;
-  if (lastKeyId) {
+  if (lastKeyId && lastModuleId) {
     url += `&LastEvaluatedKeyId=${lastKeyId}&LastEvaluatedKeyModuleId=${lastModuleId}`;
   }
   for (const id of courseIds) {

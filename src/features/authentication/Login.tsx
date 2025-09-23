ff2ccdaaccbf9ba72df7524b792551b4a3a2618e
Kristi <kwerry4@gmail.com>
2025-09-23 14:02:03 -0700

    add logouts back in

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -43,7 +43,7 @@ export default function Login(props: LoginProps): JSX.Element {
         }, 500);
       }
     } else if (!localStorage.getItem("papyrusai_access_token")) {
-      // window.location.replace(process.env.REACT_APP_LOGIN_URL ? process.env.REACT_APP_LOGIN_URL : "");
+      window.location.replace(process.env.REACT_APP_LOGIN_URL ? process.env.REACT_APP_LOGIN_URL : "");
     } else {
       navigator("/");
     }
